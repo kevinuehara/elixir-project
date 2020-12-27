370157503cde5b4e734957ea7462676281686e9c
kevin uehara <uehara.kevin@gmail.com>
2020-12-27 00:06:48 -0300

    feat: implementing ExMon Project

---
+++ ex_mon/lib/game/status.ex
@@ -0,0 +1,21 @@
+defmodule ExMon.Game.Status do
+  alias ExMon.Game
+
+  def print_round_message() do
+    IO.puts("\n======= The game is started! =======\n")
+    IO.inspect(Game.info())
+    IO.puts("                         ")
+  end
+
+  def print_wrong_move_message(move) do
+    IO.puts("\n======= Invalid move: #{move} =======\n")
+  end
+
+  def print_move_message(:computer, :attack, damage) do
+    IO.puts("\n======= The player attacked the computer dealing: #{damage} damage. =======\n")
+  end
+
+  def print_move_message(:player, :attack, damage) do
+    IO.puts("\n======= The computer attacked the player dealing: #{damage} damage. =======\n")
+  end
+end
