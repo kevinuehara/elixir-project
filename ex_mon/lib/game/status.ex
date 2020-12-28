c9d3809621bde1eea96729cc9f11fab1ac194a3e
kevin uehara <uehara.kevin@gmail.com>
2020-12-28 07:48:07 -0300

    feat: finalizing game

--- ex_mon/lib/game/status.ex
+++ ex_mon/lib/game/status.ex
@@ -1,12 +1,25 @@
 defmodule ExMon.Game.Status do
   alias ExMon.Game
 
-  def print_round_message() do
+  def print_round_message(%{status: :started}) do
     IO.puts("\n======= The game is started! =======\n")
     IO.inspect(Game.info())
     IO.puts("                         ")
   end
 
+  def print_round_message(%{status: :continue, turn: player} = info) do
+    IO.puts("\n======= It's #{player} turn. =======\n")
+    IO.inspect(info)
+    IO.puts("                         ")
+  end
+
+  def print_round_message(%{status: :game_over} = info) do
+    IO.puts("\n======= The game is over. =======\n")
+    IO.inspect(info)
+    IO.puts("                         ")
+  end
+
+  @spec print_wrong_move_message(any) :: :ok
   def print_wrong_move_message(move) do
     IO.puts("\n======= Invalid move: #{move} =======\n")
   end
@@ -17,5 +30,9 @@ defmodule ExMon.Game.Status do
 
   def print_move_message(:player, :attack, damage) do
     IO.puts("\n======= The computer attacked the player dealing: #{damage} damage. =======\n")
+  end
+
+  def print_move_message(player, :heal, life) do
+    IO.puts("\n======= The #{player} healed itself to #{life} life points. =======\n")
   end
 end
--- ex_mon/test/ex_mon_test.exs
+++ ex_mon/test/ex_mon_test.exs
@@ -1,8 +1,59 @@
 defmodule ExMonTest do
   use ExUnit.Case
-  doctest ExMon
+  import ExUnit.CaptureIO
+  alias ExMon.Player
 
-  test "greets the world" do
-    assert ExMon.hello() == :world
+  describe "create_player/4" do
+    test "returns a player" do
+      expected_reponse = %Player{life: 100, moves: %{move_avg: :chute, move_heal: :cura, move_rnd: :soco}, name: "kevin"}
+      assert expected_reponse == ExMon.create_player("kevin", :chute, :soco, :cura)
+    end
+  end
+
+  describe "start_game/1" do
+    test "when the game is started, returns a message" do
+      player = Player.build("Kevin", :chute, :soco, :cura)
+
+      messages =
+      capture_io(fn ->
+        assert ExMon.start_game(player) == :ok
+      end)
+
+      assert messages =~ "The game is started!"
+      assert messages =~ "status: :started"
+      assert messages =~ "turn: :player"
+    end
+  end
+
+  describe "make_move/1" do
+    setup do
+      player = Player.build("Kevin", :chute, :soco, :cura)
+      capture_io(fn ->
+        ExMon.start_game(player)
+      end)
+
+      :ok
+    end
+    test "when the move is valid, do the move and the computer makes a move" do
+
+      messages =
+        capture_io(fn ->
+          ExMon.make_move(:chute)
+        end)
+
+      assert messages =~ "The player attacked the computer"
+      assert messages =~ "It's computer turn"
+      assert messages =~ "It's player turn"
+      assert messages =~ "status: :continue"
+    end
+
+    test "when the move is invalid, returns an error message" do
+      messages =
+        capture_io(fn ->
+          ExMon.make_move(:wrong)
+        end)
+
+      assert messages =~ "Invalid move: wrong"
+    end
   end
 end
