68e05eddf9056aa33e1657a12c30ec16d46da8be
kevin uehara <uehara.kevin@gmail.com>
2021-03-09 11:01:26 -0300

    feat: adding web layer of exmon game

---
+++ web/ex_mon/lib/ex_mon.ex
@@ -0,0 +1,7 @@
+defmodule ExMon do
+  alias ExMon.Trainer
+
+  defdelegate create_trainer(params), to: Trainer.Create, as: :call
+  defdelegate delete_trainer(params), to: Trainer.Delete, as: :call
+  defdelegate fetch_trainer(params), to: Trainer.Get, as: :call
+end
